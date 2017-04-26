3c6843b34353d44fafc5c86c1e1780166e39ce15
Mark Meysenburg <mark.meysenburg@doane.edu>
2017-04-26 16:13:42 -0500

    modified Contours.py

--- code/08-contours/Contours.py
+++ code/08-contours/Contours.py
@@ -7,14 +7,15 @@ import cv2, sys, numpy as np
 filename = sys.argv[1]
 
 # read original image
-img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+img = cv2.imread(filename)
 
 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 #cv2.imshow("output", img)
 #cv2.waitKey(0)
 
 # create binary image
-blur = cv2.GaussianBlur(img, (5, 5), 0)
+blur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(blur, (5, 5), 0)
 (t, binary) = cv2.threshold(blur, 210, 255, cv2.THRESH_BINARY_INV)
 
 #cv2.imshow("output", binary)
@@ -24,12 +25,16 @@ blur = cv2.GaussianBlur(img, (5, 5), 0)
 (cImg, contours, hierarchy) = cv2.findContours(binary, cv2.RETR_TREE, 
 	cv2.CHAIN_APPROX_SIMPLE)
 
-print(len(contours))
+i = 0
+print("Found %d contours." % len(contours))
+for c in contours:
+	print("\tSize of contour %d: %d" % (i, len(c)))
+	i += 1
 
 # draw contours 
-final = np.zeros(binary.shape, dtype = "uint8")
-final = cv2.merge([final, final, final])
-cv2.drawContours(final, contours, -1, (255, 0, 0), 3)
+for c in contours:
+	if len(c) > 100:
+		cv2.drawContours(img, c, -1, (0, 0, 255), 3)
 
-cv2.imshow("output", final)
+cv2.imshow("output", img)
 cv2.waitKey(0)
