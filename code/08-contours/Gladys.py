aa01d5ef2f99eae91e03ea1aff9500301de404d9
Mark Meysenburg <mark.meysenburg@doane.edu>
2017-05-08 19:04:22 -0500

    prototype of pip counting works

--- code/08-contours/Gladys.py
+++ code/08-contours/Gladys.py
@@ -15,13 +15,22 @@ img = cv2.imread(filename)
 # create binary image
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
-(t, binary) = cv2.threshold(blur, t, 255, cv2.THRESH_BINARY)
+(t, binary) = cv2.threshold(blur, t, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # find contours
 (_, contours, hierarchy) = cv2.findContours(binary, cv2.RETR_TREE, 
     cv2.CHAIN_APPROX_SIMPLE)
 
-pips = 
+pips = 0
+cPrev = 0
+cCurr = hierarchy[0, 0, 0]
+while cCurr != -1:
+    pips += (cCurr - cPrev - 1)
+    cPrev = cCurr
+    cCurr = hierarchy[0, cCurr, 0]
+    
+print("Found", pips, "pips in the image.")
+'''
 # print table of contours and sizes
 for (i, c) in enumerate(hierarchy[0]):
     print(i, c, sep=':\t')
@@ -39,3 +48,4 @@ cv2.drawContours(img, contours[32:], -1, (0, 0, 255), 5)
 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 cv2.imshow("output", img)
 cv2.waitKey(0)
+'''
