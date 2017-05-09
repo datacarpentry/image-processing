c2c5c5fac67d519a89214d69593435e46971f681
Mark M. Meysenburg <mark.meysenburg@doane.edu>
2017-05-09 14:32:59 -0500

    finished pip count challenge

--- code/08-contours/Gladys.py
+++ code/08-contours/Gladys.py
@@ -20,53 +20,8 @@ blur = cv2.GaussianBlur(gray, (5, 5), 0)
 # find contours
 (_, contours, hierarchy) = cv2.findContours(binary, cv2.RETR_TREE, 
     cv2.CHAIN_APPROX_SIMPLE)
-print(hierarchy.shape)
 
-outers = [0, 6, 11, 17, 23, 27, 32]
-pips = [1, 2, 3, 4, 5, 7, 8, 9, 10, 12, 15, 16, 18, 19, 20, 21, 22,
-        24, 25, 26, 28, 29, 30, 31, 33, 34, 35]
-inners = [13, 14]
-
-for i in outers:
-    cv2.drawContours(img, contours, i, (0, 0, 255), 5)
-    
-for i in pips:
-    cv2.drawContours(img, contours, i, (0, 255, 0), 5)
-    
-for i in inners:
-    cv2.drawContours(img, contours, i, (255, 0, 0), 5)
-    
-cv2.namedWindow("contours", cv2.WINDOW_NORMAL)
-cv2.imshow("contours", img)
-cv2.waitKey(0)
-'''
-pips = 0
-cPrev = 0
-cCurr = hierarchy[0, 0, 0]
-while cCurr != -1:
-    pips += (cCurr - cPrev - 1)
-    cPrev = cCurr
-    cCurr = hierarchy[0, cCurr, 0]
-pips += (hierarchy.shape[1] - cPrev - 1)
-    
-print("Found", pips, "pips in the image.")
-'''
-'''
-# print table of contours and sizes
-for (i, c) in enumerate(hierarchy[0]):
-    print(i, c, sep=':\t')
-
-# draw contours over original image
-cv2.drawContours(img, contours[0:7], -1, (0, 0, 255), 5)
-cv2.drawContours(img, contours[6:12], -1, (255, 0, 0), 5)
-cv2.drawContours(img, contours[11:18], -1, (0, 0, 255), 5)
-cv2.drawContours(img, contours[17:24], -1, (255, 0, 0), 5)
-cv2.drawContours(img, contours[23:28], -1, (0, 0, 255), 5)
-cv2.drawContours(img, contours[27:33], -1, (255, 0, 0), 5)
-cv2.drawContours(img, contours[32:], -1, (0, 0, 255), 5)
-
-# display original image with contours
-cv2.namedWindow("output", cv2.WINDOW_NORMAL)
-cv2.imshow("output", img)
-cv2.waitKey(0)
-'''
+# Count the number of pips on the dice faces.
+# Iterate through hierarchy[0], first to find the indices of dice
+# contours, then again to find pip contours.
+# WRITE YOUR CODE HERE
