55ba20ef89a0cca686aeeba9859bc406b75e17f7
Constantin Pape <c.pape@gmx.net>
2019-09-01 20:09:56 +0200

    Update 03 code examples

---
+++ code/03-skimage-images/HighIntensity.py
@@ -0,0 +1,22 @@
+'''
+* Python script to ignore low intensity pixels in an image.
+*
+* usage: python HighIntensity.py <filename>
+'''
+import sys
+import skimage.io
+import skimage.viewer
+
+# read input image, based on filename parameter
+image = skimage.io.imread(fname=sys.argv[1])
+
+# display original image
+viewer = skimage.viewer.ImageViewer(image)
+viewer.show()
+
+# keep only high-intensity pixels
+image[image < 128] = 0
+
+# display modified image
+viewer = skimage.viewer.ImageViewer(image)
+viewer.show()
