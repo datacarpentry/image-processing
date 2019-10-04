ef3906a07df847b3e9374d07251f26adf924cae0
Erin Becker <erinstellabecker@gmail.com>
2019-10-04 15:01:13 -0700

    add space around operators

--- code/05-creating-histograms/GrayscaleMaskHistogram.py
+++ code/05-creating-histograms/GrayscaleMaskHistogram.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 
 # read image, based on command line filename argument;
 # read the image as grayscale from the outset
-image = skimage.io.imread(fname=sys.argv[1], as_gray=True)
+image = skimage.io.imread(fname = sys.argv[1], as_gray = True)
 
 # display the image
 viewer = skimage.viewer.ImageViewer(image)
@@ -22,7 +22,7 @@ viewer.show()
 
 # apply the mask to the input image, create the histogram
 # MODIFY CODE HERE
-histogram = np.histogram(image, bins=256, range=(0, 256))
+histogram = np.histogram(image, bins = 256, range = (0, 256))
 
 # configure and draw the histogram figure
 plt.figure()
