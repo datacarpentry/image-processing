6e3fc075e21bf6473814e31c6f0512b1b84ce1b0
Erin Becker <erinstellabecker@gmail.com>
2019-10-04 15:00:27 -0700

    add spaces around operators

--- code/05-creating-histograms/GrayscaleHistogram.py
+++ code/05-creating-histograms/GrayscaleHistogram.py
@@ -11,14 +11,14 @@ from matplotlib import pyplot as plt
 
 # read image, based on command line filename argument;
 # read the image as grayscale from the outset
-image = skimage.io.imread(fname=sys.argv[1], as_gray=True)
+image = skimage.io.imread(fname = sys.argv[1], as_gray = True)
 
 # display the image
 viewer = skimage.viewer.ImageViewer(image)
 viewer.show()
 
 # create the histogram
-histogram = np.histogram(image, bins=256, range=(0, 256))
+histogram = np.histogram(image, bins = 256, range = (0, 256))
 
 # configure and draw the histogram figure
 plt.figure()
