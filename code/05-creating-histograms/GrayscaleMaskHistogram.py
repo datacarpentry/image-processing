f82b2b0955f57f02bf6ac0f763ecd6034d0867f5
k-dominik <k-dominik@users.noreply.github.com>
2019-11-10 11:44:23 +0100

    applied black coding style

--- code/05-creating-histograms/GrayscaleMaskHistogram.py
+++ code/05-creating-histograms/GrayscaleMaskHistogram.py
@@ -1,7 +1,7 @@
-'''
+"""
  * Generate a grayscale histogram for an image.
  * Usage: python GrayscaleMaskHistogram.py <filename>
-'''
+"""
 import sys
 import skimage.draw
 import skimage.io
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 
 # read image, based on command line filename argument;
 # read the image as grayscale from the outset
-image = skimage.io.imread(fname = sys.argv[1], as_gray = True)
+image = skimage.io.imread(fname=sys.argv[1], as_gray=True)
 
 # display the image
 viewer = skimage.viewer.ImageViewer(image)
@@ -22,7 +22,7 @@ viewer.show()
 
 # apply the mask to the input image, create the histogram
 # MODIFY CODE HERE
-histogram, bin_edges = np.histogram(image, bins = 256, range = (0., 1.))
+histogram, bin_edges = np.histogram(image, bins=256, range=(0.0, 1.0))
 
 # configure and draw the histogram figure
 plt.figure()
