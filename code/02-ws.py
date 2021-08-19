eecff9211d97ffc395b4a1ad0ab7054683b863c8
Toby Hodges <tbyhdgs@gmail.com>
2021-08-19 11:18:03 +0200

    update ws.py to write to data folder

--- code/02-ws.py
+++ code/02-ws.py
@@ -2,7 +2,7 @@
  * Python program to create an image containing a single, white square.
  * The image is saved in two formats: bmp and jpg.
  *
- * usage: python ws.py <dim>
+ * usage: python 02-ws.py <dim>
 """
 import skimage.io
 import sys
@@ -13,5 +13,5 @@ dim = int(sys.argv[1])
 img = np.zeros((dim, dim, 3), dtype="uint8")
 img.fill(255)
 
-skimage.io.imsave(fname="ws.bmp", arr=img)
-skimage.io.imsave(fname="ws.jpg", arr=img)
+skimage.io.imsave(fname="data/02-ws.bmp", arr=img)
+skimage.io.imsave(fname="data/02-ws.jpg", arr=img)
