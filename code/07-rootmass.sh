2514aaf1a3b92fe1837bdd7ff01c140c82ca46a2
Toby Hodges <tbyhdgs@gmail.com>
2021-08-19 12:15:27 +0200

    update script to reflect new filepaths

--- code/07-rootmass.sh
+++ code/07-rootmass.sh
@@ -2,10 +2,10 @@
 # Run the root density mass on all of the root system trail images.
 
 # first, remove existing binary output images
-rm *-binary.jpg
+rm data/*-binary.jpg
 
 # then, execute the program on all the trail images
-for f in trial-*.jpg
+for f in data/07-trial-*.jpg
 do
-	python RootMass.py $f 1.5
+	python code/07-RootMass.py $f 1.5
 done
